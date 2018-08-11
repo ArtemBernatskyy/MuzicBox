adc802257796c9bc48b204de2a20eb88bf414bcd
Artem Bernatskyi <artem.bernatskyy@gmail.com>
2018-08-11 11:48:27 +0300

    added frontend Sentry integration

--- reactmusic/apps/landing/views.py
+++ reactmusic/apps/landing/views.py
@@ -1,5 +1,11 @@
+from django.conf import settings
 from django.views.generic import TemplateView
 
 
 class MainPageView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(MainPageView, self).get_context_data(**kwargs)
+        context['debug'] = settings.DEBUG
+        return context

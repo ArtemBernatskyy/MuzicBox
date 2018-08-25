8cc937c67ce8a32325743e24232c2122f17d0ed0
Artem Bernatskyi <artem.bernatskyy@gmail.com>
2018-08-25 14:35:29 +0300

    added better FE reporting, separated devDep

--- front_end/src/store/configureStore.dev.js
+++ front_end/src/store/configureStore.dev.js
@@ -1,17 +1,20 @@
-import { createStore, applyMiddleware } from "redux";
-import { createLogger } from "redux-logger";
+import Raven from "raven-js";
 import thunk from "redux-thunk";
+import { createLogger } from "redux-logger";
+import createRavenMiddleware from "raven-for-redux";
+import { createStore, applyMiddleware } from "redux";
 
 import rootReducer from "reducers";
 
 const logger = createLogger();
-
 const createStoreWithMiddleware = applyMiddleware(logger)(createStore);
+
+Raven.config("https://e4324708c61040909daaed217fc879f5@sentry.io/1258839").install();
 
 export default function configureStore() {
   return createStoreWithMiddleware(
     rootReducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    applyMiddleware(thunk)
+    applyMiddleware(thunk, createRavenMiddleware(Raven, {}))
   );
 }
